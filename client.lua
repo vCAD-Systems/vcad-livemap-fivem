745adfbfe4c3976e81f069a3a55f628349680992
Ffrankys <FfrankysZockt@gmail.com>
2022-12-02 21:59:48 +0100

    Improvements / Performance Changes

--- client.lua
+++ client.lua
@@ -2,34 +2,31 @@ local isinveh = false
 local vehicle = nil
 local type = ""
 Citizen.CreateThread(function()
-
     while true do
+        local playerPed = PlayerPedId()
 
-        if IsPedInAnyVehicle(PlayerPedId(), false) then
-            if not isinveh then
-                isinveh = true
-                vehicle = GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), false))
-                type = ""
-                if IsThisModelACar(vehicle) then
-                    type = "car"
-                end
-                if IsThisModelAHeli(vehicle) then
-                    type = "heli"
-                end
-                if IsThisModelAPlane(vehicle) then
-                    type = "aircraft"
-                end
-                TriggerServerEvent("vcad-livemap:vehicle_entered", vehicle, type)
-            end 
-        else
-            if isinveh then
-                isinveh = false
-                vehicle = nil
-                TriggerServerEvent("vcad-livemap:vehicle_left")
+        if not isinveh and IsPedInAnyVehicle(playerPed, false) then
+            vehicle = GetEntityModel(GetVehiclePedIsIn(playerPed, false))
+            isinveh = true
+            type = ""
+            
+            if IsThisModelACar(vehicle) then
+                type = "car"
             end
+            if IsThisModelAHeli(vehicle) then
+                type = "heli"
+            end
+            if IsThisModelAPlane(vehicle) then
+                type = "aircraft"
+            end
+
+            TriggerServerEvent("vcad-livemap:vehicle_entered", vehicle, type)
+        elseif isinveh then
+            isinveh = false
+            vehicle = nil
+            TriggerServerEvent("vcad-livemap:vehicle_left")
         end
 
         Citizen.Wait(1000)
-
     end
 end)
